14c5266384372c51307c472b8e432554c6d6e63a
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-27 16:27:31 +0200

    Added migration values

--- database/migrations/2023_07_27_140448_create_beaches_table.php
+++ database/migrations/2023_07_27_140448_create_beaches_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('beaches', function (Blueprint $table) {
             $table->id();
+            $table->name();
+            $table->city();
+            $table->n_umbrellas();
+            $table->n_seats();
+            $table->umbrellas_day_price();
+            $table->opening_date();
+            $table->closing_date();
+            $table->has_volley();
+            $table->has_football();
             $table->timestamps();
         });
     }
