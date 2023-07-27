3054d4df201cf02e454c20a6f52623e243fc4b9c
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-27 18:32:38 +0200

    populated table with db model infos

--- resources/views/admin/beaches/index.blade.php
+++ resources/views/admin/beaches/index.blade.php
@@ -3,7 +3,58 @@
 @section('title', 'Beaches Admin List')
 
 @section('admin-content')
-    <h1>
-        Beaches Admin Index
-    </h1>
+<div class="container">
+    <div class="row">
+        <table class="table table-dark table-hover table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">City</th>
+                <th scope="col">N. Umbrellas</th>
+                <th scope="col">N. Seats</th>
+                <th scope="col">Day Price</th>
+                <th scope="col">Opening Date</th>
+                <th scope="col">Closing Date</th>
+                <th scope="col">Volley</th>
+                <th scope="col">Football</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($beaches as $beach)
+                    <tr>
+                        <th scope="row">{{ $beach->id }}</th>
+                        <td>{{ $beach->name }}</td>
+                        <td>{{ $beach->city }}</td>
+                        <td>{{ $beach->n_umbrellas }}</td>
+                        <td>{{ $beach->n_seats }}</td>
+                        <td>${{ $beach->umbrellas_day_price }}</td>
+                        <td>{{ $beach->opening_date }}</td>
+                        <td>{{ $beach->closing_date }}</td>
+                        <td>{{ $beach->has_volley }}</td>
+                        <td>{{ $beach->has_football }}</td>
+                        <td class="d-flex gap-1">
+                            <a href="" class="btn btn-warning">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                            <a href="" class="btn btn-info">
+                                <i class="fa-solid fa-pen"></i>
+                            </a>
+                            <form action="" method="post">
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+          </table>
+    </div>
+
+    <div class="row">
+        {!! $beaches->links() !!}
+    </div>
+</div>
 @endsection
