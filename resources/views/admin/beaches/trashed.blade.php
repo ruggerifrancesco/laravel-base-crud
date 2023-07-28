0852fba288e060ce3014a1ca73a3f53f59a98095
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-07-28 18:11:22 +0200

    delete

--- resources/views/admin/beaches/trashed.blade.php
+++ resources/views/admin/beaches/trashed.blade.php
@@ -76,8 +76,13 @@
 @section('custom-script-tail')
 <script>
     const deleteForm = document.querySelectorAll('form-delete');
-    deleteForm.addEventListener(('submit'), function(event) {
-
-    })
+    deleteForm.foreach(formElement=>) {
+       formElement.addEventListener('submit, function(event){
+        event.preventDeFault();
+        const userConfirm=window.confirm('Are you sure you want to delete this beach?');
+        if(userConfirm){
+            this.submit();
+        }
+    });
 </script>
 @endsection
